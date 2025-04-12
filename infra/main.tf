2a448023488e93d6e4ef256dba2b528c3bc68d37
reyrahadian <rahadian.rey@gmail.com>
2025-04-12 19:29:05 +1000

    feat: increase resources as the container keeps running out of memory

--- infra/main.tf
+++ infra/main.tf
@@ -571,8 +571,8 @@ resource "aws_ecs_task_definition" "extractor" {
   family                   = "${var.application_name}-extractor-${var.environment}"
   requires_compatibilities = ["FARGATE"]
   network_mode            = "awsvpc"
-  cpu                     = 256
-  memory                  = 512
+  cpu                     = "1024"
+  memory                  = "2048"
   execution_role_arn      = aws_iam_role.ecs_execution_role.arn
   task_role_arn           = aws_iam_role.ecs_task_role.arn
 
