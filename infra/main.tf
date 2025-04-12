5e2b64dd274d3b3e4bece563be7e9b2e5d311ccf
reyrahadian <rahadian.rey@gmail.com>
2025-04-12 19:50:21 +1000

    chore: use AEST

--- infra/main.tf
+++ infra/main.tf
@@ -806,7 +806,7 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Create EventBridge scheduler for scaling up extractor at 8am
+# Create EventBridge scheduler for scaling up extractor at 8am AEST
 resource "aws_scheduler_schedule" "extractor_scale_up_8am" {
   name       = "${var.application_name}-extractor-scale-up-8am-${var.environment}"
   group_name = "default"
@@ -815,7 +815,7 @@ resource "aws_scheduler_schedule" "extractor_scale_up_8am" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(0 8 ? * MON-FRI *)"  # 8 AM UTC on weekdays
+  schedule_expression = "cron(0 22 ? * SUN-THU *)"  # 8 AM AEST on weekdays (22:00 UTC previous day)
 
   target {
     arn      = aws_lambda_function.update_ecs_task_count.arn
@@ -828,7 +828,7 @@ resource "aws_scheduler_schedule" "extractor_scale_up_8am" {
   }
 }
 
-# Create EventBridge scheduler for scaling down extractor at 10am
+# Create EventBridge scheduler for scaling down extractor at 10am AEST
 resource "aws_scheduler_schedule" "extractor_scale_down_10am" {
   name       = "${var.application_name}-extractor-scale-down-10am-${var.environment}"
   group_name = "default"
@@ -837,7 +837,7 @@ resource "aws_scheduler_schedule" "extractor_scale_down_10am" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(0 10 ? * MON-FRI *)"  # 10 AM UTC on weekdays
+  schedule_expression = "cron(0 0 ? * MON-FRI *)"  # 10 AM AEST on weekdays (00:00 UTC)
 
   target {
     arn      = aws_lambda_function.update_ecs_task_count.arn
@@ -895,7 +895,7 @@ resource "aws_iam_role_policy" "eventbridge_lambda_policy" {
   })
 }
 
-# Create EventBridge scheduler for scaling up reporting app at 8am
+# Create EventBridge scheduler for scaling up reporting app at 8am AEST
 resource "aws_scheduler_schedule" "reporting_scale_up_8am" {
   name       = "${var.application_name}-reporting-scale-up-8am-${var.environment}"
   group_name = "default"
@@ -904,7 +904,7 @@ resource "aws_scheduler_schedule" "reporting_scale_up_8am" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(0 8 ? * MON-FRI *)"  # 8 AM UTC on weekdays
+  schedule_expression = "cron(0 22 ? * SUN-THU *)"  # 8 AM AEST on weekdays (22:00 UTC previous day)
 
   target {
     arn      = aws_lambda_function.update_ecs_task_count.arn
@@ -917,7 +917,7 @@ resource "aws_scheduler_schedule" "reporting_scale_up_8am" {
   }
 }
 
-# Create EventBridge scheduler for scaling down reporting app at 6pm
+# Create EventBridge scheduler for scaling down reporting app at 6pm AEST
 resource "aws_scheduler_schedule" "reporting_scale_down_6pm" {
   name       = "${var.application_name}-reporting-scale-down-6pm-${var.environment}"
   group_name = "default"
@@ -926,7 +926,7 @@ resource "aws_scheduler_schedule" "reporting_scale_down_6pm" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(0 18 ? * MON-FRI *)"  # 6 PM UTC on weekdays
+  schedule_expression = "cron(0 8 ? * MON-FRI *)"  # 6 PM AEST on weekdays (08:00 UTC)
 
   target {
     arn      = aws_lambda_function.update_ecs_task_count.arn
