485eb0aec545b14a66913ffee0ca0ac4e19d8bf3
reyrahadian <rahadian.rey@gmail.com>
2025-04-17 17:17:38 +1000

    chore: add unit tests for string_utils.py

--- apps/reporting_app/src/utils/string_utils.py
+++ apps/reporting_app/src/utils/string_utils.py
@@ -1,16 +1,12 @@
-def split_string_array(string_array):
-    # example values
-    # ["2025-01-21T23:31:33.421Z"-"2025-02-04T23:59:43.560Z"]
-    # ["2024-10-01T09:00:00.000Z"-null]
-    # [null-"2024-02-21T00:39:33.758Z"-"2023-11-15T03:11:17.715Z"]
-    if string_array.startswith('['):
-        list_str = string_array.strip('[]').replace("null","\"null\"")
-        sprints = [s.replace('"', '').strip() for s in list_str.split("\"-\"")]
-        return sprints
+def split_string_array(value:str, separator:str)->list[str]:
+    if is_in_array(value):
+        list_str = value.strip('[]').replace("null","\"null\"")
+        values = [s.replace('"', '').strip() for s in list_str.split(separator)]
+        return values
 
-    return [string_array]
+    return [value]
 
-def is_multiple_values(value):
+def is_in_array(value:str)->bool:
         if '[' in value:
             return True
         return False
---
+++ apps/reporting_app/tests/utils/test_string_utils.py
@@ -0,0 +1,17 @@
+from src.utils.string_utils import split_string_array, is_in_array
+
+def test_split_string_array():
+    assert split_string_array("API,BFF,BFF-Web,SFCC", ",") == ['API,BFF,BFF-Web,SFCC']
+    assert split_string_array("[API]", ",") == ["API"]
+    assert split_string_array("[API,BFF,BFF-Web]", ",") == ["API","BFF","BFF-Web"]
+    assert split_string_array("[API,BFF,BFF-Web,SFCC]", ",") == ["API","BFF","BFF-Web","SFCC"]
+    assert split_string_array('["2025-01-21T23:31:33.421Z"-"2025-02-04T23:59:43.560Z"]', '"-"') == ["2025-01-21T23:31:33.421Z","2025-02-04T23:59:43.560Z"]
+    assert split_string_array('["2024-10-01T09:00:00.000Z"-null]', '"-"') == ["2024-10-01T09:00:00.000Z","null"]
+    assert split_string_array('[null-"2024-02-21T00:39:33.758Z"-"2023-11-15T03:11:17.715Z"]', '"-"') == ["null","2024-02-21T00:39:33.758Z","2023-11-15T03:11:17.715Z"]
+
+def test_is_in_array():
+    assert is_in_array("[API]") == True
+    assert is_in_array("API") == False
+    assert is_in_array("API,BFF,BFF-Web,SFCC") == False
+    assert is_in_array("API,BFF,BFF-Web,SFCC") == False
+    assert is_in_array("API,BFF,BFF-Web,SFCC") == False
