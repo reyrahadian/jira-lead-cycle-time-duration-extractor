ac4721a7874172062063490dfa68bfe6825e5db6
reyrahadian <rahadian.rey@gmail.com>
2025-05-27 15:24:07 +1000

    feat: remove obsolete teams dashboard

--- apps/reporting_app/app.py
+++ apps/reporting_app/app.py
@@ -6,8 +6,6 @@ from src.components.tabs.sprint_dashboard.callbacks \
 from src.data.data_loaders import JiraDataSingleton
 from src.components.tabs.sprint_dashboard.components.header import create_header
 from src.components.tabs.sprint_dashboard.sprint_tab import create_sprint_tab
-from src.components.tabs.teams_dashboard.teams_tab import create_teams_tab
-from src.components.tabs.teams_dashboard.callbacks import chart_callbacks as teams_chart_callbacks
 from src.components.tabs.dora_dashboard.dora_tab import create_dora_tab
 from src.components.tabs.dora_dashboard.callbacks import filters_callbacks as dora_filters_callbacks
 from src.components.tabs.dora_dashboard.callbacks import dora_tiles_callbacks
@@ -40,7 +38,6 @@ app.layout = html.Div([
     dbc.Tabs([
         create_sprint_tab(jira_data),
         create_dora_tab(jira_data)
-        #create_teams_tab(jira_data),
     ], id='tabs-component',style={'margin-top': '10px'}),
 
     # Add a placeholder for the notification
@@ -57,7 +54,6 @@ defects_table_callbacks.init_callbacks(app, jira_data.get_tickets())
 sprint_tickets_callbacks.init_callbacks(app, jira_data.get_tickets())
 dora_filters_callbacks.init_callbacks(app, jira_data.get_tickets())
 dora_tiles_callbacks.init_callbacks(app, jira_data.get_tickets())
-#teams_chart_callbacks.init_callbacks(app, jira_data.get_tickets())
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, port=8050)
