d0dc5cf4d3840d291ca6424ec83a7c354847ecee
reyrahadian <rahadian.rey@gmail.com>
2025-06-01 07:28:50 +1000

    chore: update unit tests

--- apps/reporting_app/tests/utils/test_string_utils.py
+++ apps/reporting_app/tests/utils/test_string_utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from src.utils.string_utils import split_string_array, is_in_array
 
 def test_split_string_array():
@@ -8,6 +9,7 @@ def test_split_string_array():
     assert split_string_array('["2025-01-21T23:31:33.421Z"-"2025-02-04T23:59:43.560Z"]', '"-"') == ["2025-01-21T23:31:33.421Z","2025-02-04T23:59:43.560Z"]
     assert split_string_array('["2024-10-01T09:00:00.000Z"-null]', '"-"') == ["2024-10-01T09:00:00.000Z","null"]
     assert split_string_array('[null-"2024-02-21T00:39:33.758Z"-"2023-11-15T03:11:17.715Z"]', '"-"') == ["null","2024-02-21T00:39:33.758Z","2023-11-15T03:11:17.715Z"]
+    assert split_string_array('["D.A.W.N - Sprint 10.2.25"-"D.A.W.N - Sprint 11.2.25"]', '"-"') == ["D.A.W.N - Sprint 10.2.25","D.A.W.N - Sprint 11.2.25"]
 
 def test_is_in_array():
     assert is_in_array("[API]") == True
