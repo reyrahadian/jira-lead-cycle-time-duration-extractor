b97f0ff6abd95eeb23a9b13b76915597a56e34fb
reyrahadian <rahadian.rey@gmail.com>
2025-06-12 10:17:29 +1000

    chore: remove health check and update docker file

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -35,14 +35,6 @@ resource "aws_ecs_task_definition" "reporting" {
       image     = "${aws_ecr_repository.reporting.repository_url}:latest"
       essential = true
 
-      healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:8050/ || exit 1"]
-        interval    = 30
-        timeout     = 5
-        retries     = 3
-        startPeriod = 10
-      }
-
       mountPoints = [
         {
           sourceVolume  = "efs-data"
